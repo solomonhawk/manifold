ba5b3b8d0eab927eb5e632eeb0ba53a5327264eb
Solomon Hawk <solomon.hawk@viget.com>
2024-10-12 00:17:23 -0400

    Add turbo ui/component generator

---
+++ turbo/generators/config.ts
@@ -0,0 +1,33 @@
+import { PlopTypes } from "@turbo/gen";
+import isPascalCase from "@stdlib/assert-is-pascalcase";
+
+export default function generator(plop: PlopTypes.NodePlopAPI): void {
+  plop.setGenerator("ui/component", {
+    description: "A new UI component",
+    prompts: [
+      {
+        type: "input",
+        name: "component_name",
+        message: "What is the name of the new component?",
+        validate: (input: string) => {
+          if (!input) {
+            return "file name is required";
+          }
+
+          if (!isPascalCase(input)) {
+            return "file name must be in PascalCase";
+          }
+
+          return true;
+        },
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "{{ turbo.paths.root }}/packages/ui/src/components/{{ dashCase component_name }}.tsx",
+        templateFile: "templates/ui/component/index.hbs",
+      },
+    ],
+  });
+}
