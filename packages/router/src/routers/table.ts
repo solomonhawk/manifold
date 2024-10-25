4f1f955fd2b360b356df3f87bbaff972cd0a1c44
Solomon Hawk <solomon.hawk@viget.com>
2024-10-25 11:46:17 -0400

    Add table list, update, and get routes, update validator

--- packages/router/src/routers/table.ts
+++ packages/router/src/routers/table.ts
@@ -1,14 +1,57 @@
-import { db, schema } from "@manifold/db";
-import { tableCreateInput } from "@manifold/validators";
+import { and, db, desc, eq, schema } from "@manifold/db";
+import { tableCreateInput, tableUpdateInput, z } from "@manifold/validators";
 
 import { authedProcedure, t } from "#trpc.ts";
 
 export const tableRouter = t.router({
-  create: authedProcedure.input(tableCreateInput).mutation(({ input, ctx }) => {
-    return db
-      .insert(schema.table)
-      .values({ ...input, userId: ctx.user.id })
-      .returning()
+  list: authedProcedure.query(({ ctx }) => {
+    return db.query.table
+      .findMany({
+        where: eq(schema.table.userId, ctx.user.id),
+        orderBy: desc(schema.table.updatedAt),
+      })
+      .execute();
+  }),
+
+  create: authedProcedure
+    .input(tableCreateInput)
+    .mutation(async ({ input, ctx }) => {
+      const [table] = await db
+        .insert(schema.table)
+        .values({ ...input, userId: ctx.user.id })
+        .returning()
+        .execute();
+
+      return table;
+    }),
+
+  update: authedProcedure
+    .input(tableUpdateInput)
+    .mutation(async ({ input, ctx }) => {
+      // @TODO: differentiate between not found and not allowed?
+      const [table] = await db
+        .update(schema.table)
+        .set({ ...input, updatedAt: new Date() })
+        .where(
+          and(
+            eq(schema.table.userId, ctx.user.id),
+            eq(schema.table.id, input.id),
+          ),
+        )
+        .returning()
+        .execute();
+
+      return table;
+    }),
+
+  get: authedProcedure.input(z.string()).query(({ input, ctx }) => {
+    return db.query.table
+      .findFirst({
+        where: and(
+          eq(schema.table.userId, ctx.user.id),
+          eq(schema.table.id, input),
+        ),
+      })
       .execute();
   }),
 });
--- packages/validators/src/index.ts
+++ packages/validators/src/index.ts
@@ -2,6 +2,14 @@ import type { TableInsert } from "@manifold/db/schema/table";
 import { z, type ZodType } from "zod";
 
 export const tableCreateInput = z.object({
-  title: z.string(),
+  title: z.string().min(1, { message: "Title can’t be blank" }),
   definition: z.string(),
 }) satisfies ZodType<Omit<TableInsert, "userId">>;
+
+export const tableUpdateInput = z.object({
+  id: z.string(),
+  title: z.string().min(1).optional(),
+  definition: z.string().optional(),
+}) satisfies ZodType<Partial<TableInsert>>;
+
+export * from "zod";
