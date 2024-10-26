e3ebd2df0af3174e092f550c41cdd0eb0c5dec8b
Solomon Hawk <solomon.hawk@viget.com>
2024-10-26 19:48:06 -0400

    Animate quick launcher tiles

--- apps/web/src/features/dashboard/components/quick-launcher/index.tsx
+++ apps/web/src/features/dashboard/components/quick-launcher/index.tsx
@@ -1,7 +1,9 @@
 import { Button } from "@manifold/ui/components/ui/button";
 import { Card, CardContent } from "@manifold/ui/components/ui/card";
+import { AnimatePresence, motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
+import { transitionAlpha } from "~utils/animation";
 import { trpc } from "~utils/trpc";
 
 export function QuickLauncher() {
@@ -13,24 +15,34 @@ export function QuickLauncher() {
   }
 
   return (
-    <section className="flex gap-12 sm:gap-16">
-      {data.map((table) => {
-        return <QuickLaunchTile key={table.title} table={table} />;
-      })}
-    </section>
+    <AnimatePresence initial={false} mode="popLayout">
+      <ul className="flex gap-6 sm:gap-8">
+        {data.map((table) => {
+          return <QuickLaunchTile key={table.title} table={table} />;
+        })}
+      </ul>
+    </AnimatePresence>
   );
 }
 
 function QuickLaunchTile({ table }: { table: { id: string; title: string } }) {
   return (
-    <Card>
-      <CardContent className="!p-0 flex items-center h-full">
-        <Button asChild className="w-full h-full p-24" variant="link">
-          <Link to={`/table/${table.id}`}>
-            <h3>{table.title}</h3>
-          </Link>
-        </Button>
-      </CardContent>
-    </Card>
+    <motion.li
+      layout
+      transition={transitionAlpha}
+      initial={{ opacity: 0, scale: 0.9 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ opacity: 0, scale: 0.9 }}
+    >
+      <Card>
+        <CardContent className="!p-0 flex items-center h-full">
+          <Button asChild className="w-full h-full p-16" variant="link">
+            <Link to={`/table/${table.id}`}>
+              <h3>{table.title}</h3>
+            </Link>
+          </Button>
+        </CardContent>
+      </Card>
+    </motion.li>
   );
 }
