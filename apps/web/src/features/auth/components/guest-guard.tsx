3ee0c545503c12477afeb714e5f272bbb810200f
Solomon Hawk <solomon.hawk@viget.com>
2024-10-26 02:11:28 -0400

    Add loading indicator to GuestGuard

--- apps/web/src/features/auth/components/guest-guard.tsx
+++ apps/web/src/features/auth/components/guest-guard.tsx
@@ -1,12 +1,17 @@
 import { useSession } from "@manifold/auth/client";
+import { LoadingIndicator } from "@manifold/ui/components/loading-indicator";
+import { FlexCol } from "@manifold/ui/components/ui/flex";
 import { Navigate, Outlet } from "react-router-dom";
 
 export function GuestGuard() {
   const { status } = useSession();
 
   if (status === "loading") {
-    // @TODO: better loading state, suspense, router?
-    return null;
+    return (
+      <FlexCol className="items-center justify-center">
+        <LoadingIndicator />
+      </FlexCol>
+    );
   }
 
   if (status === "unauthenticated") {
