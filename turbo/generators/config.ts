07d454d32caa6e84b272f3e7fc8bd4bb5f9a669b
Solomon Hawk <solomon.hawk@viget.com>
2024-11-21 01:51:39 -0500

    Update component generators

--- turbo/generators/config.ts
+++ turbo/generators/config.ts
@@ -6,6 +6,13 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
     description: "A new UI component",
     prompts: [
       {
+        type: "confirm",
+        name: "core",
+        message: "Is this a core component?",
+        default: false,
+      },
+
+      {
         type: "input",
         name: "component_name",
         message: "What is the name of the new component?",
@@ -22,12 +29,54 @@ export default function generator(plop: PlopTypes.NodePlopAPI): void {
         },
       },
     ],
-    actions: [
-      {
-        type: "add",
-        path: "{{ turbo.paths.root }}/packages/ui/src/components/{{ dashCase component_name }}.tsx",
-        templateFile: "templates/ui/component/index.hbs",
-      },
-    ],
+
+    actions: (asdf) => {
+      const isCoreComponent = asdf?.core ?? false;
+
+      if (isCoreComponent) {
+        return [
+          // components/core/<component>/index.ts
+          {
+            type: "add",
+            path: "{{ turbo.paths.root }}/packages/ui/src/components/core/{{ dashCase component_name }}/index.ts",
+            templateFile: "templates/ui/component/core/index.ts.hbs",
+          },
+          // components/core/<component>/<component>.tsx
+          {
+            type: "add",
+            path: "{{ turbo.paths.root }}/packages/ui/src/components/core/{{ dashCase component_name }}/{{ dashCase component_name }}.tsx",
+            templateFile: "templates/ui/component/core/component.tsx.hbs",
+          },
+          // components/core/<component>/<component>.stories.tsx
+          {
+            type: "add",
+            path: "{{ turbo.paths.root }}/packages/ui/src/components/core/{{ dashCase component_name }}/{{ dashCase component_name }}.stories.tsx",
+            templateFile:
+              "templates/ui/component/core/component.stories.tsx.hbs",
+          },
+        ];
+      }
+
+      return [
+        // components/<component>/index.ts
+        {
+          type: "add",
+          path: "{{ turbo.paths.root }}/packages/ui/src/components/{{ dashCase component_name }}/index.ts",
+          templateFile: "templates/ui/component/index.ts.hbs",
+        },
+        // components/<component>/<component>.tsx
+        {
+          type: "add",
+          path: "{{ turbo.paths.root }}/packages/ui/src/components/{{ dashCase component_name }}/{{ dashCase component_name }}.tsx",
+          templateFile: "templates/ui/component/component.tsx.hbs",
+        },
+        // components/<component>/<component>.stories.tsx
+        {
+          type: "add",
+          path: "{{ turbo.paths.root }}/packages/ui/src/components/{{ dashCase component_name }}/{{ dashCase component_name }}.stories.tsx",
+          templateFile: "templates/ui/component/component.stories.tsx.hbs",
+        },
+      ];
+    },
   });
 }
